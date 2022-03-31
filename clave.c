b7483a471b153e293a0ba01fd9c362013d4cf536
Julicss <julietaperez762@gmail.com>
2022-03-31 20:12:54 -0300

    borrar esto

---
+++ clave.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+main(){
+	
+	int clave;
+  
+         	  
+			   
+		   printf("Ingrese su clave:");
+			scanf("%d",&clave);  			
+
+
+	if (clave==23645){
+          	
+         printf("\n\nLa clave es correcta");	
+        
+		}
+      
+     
+      else {
+      	
+      	 printf("\n\nLa clave es incorrecta" );	
+	  }		
+		
+	
+	return 0;
+	
+}
